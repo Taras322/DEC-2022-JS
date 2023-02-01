08c8c93a2fe3bbf58749dda0afcf66aa28095058
Taras322 <taraslasiv@gmail.com>
2023-02-01 19:01:55 +0200

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,68 @@
+let i = 'hello';
+console.log(i);
+
+let o = 'owu';
+console.log(o);
+
+let l = 'com';
+console.log(l);
+
+let d = 'ua';
+console.log(d);
+
+let e = 1;
+console.log(e);
+
+let f = 10;
+console.log(f);
+
+let j = -999;
+console.log(j);
+
+let k = 123;
+console.log(k);
+
+let pi = 3.14;
+console.log(pi);
+
+let el = 2.7;
+console.log(el);
+
+let p = 16;
+console.log(p);
+
+let t = true;
+console.log(t);
+
+let fal = false;
+console.log(fal);
+
+//створив змінні і вивів кожну змінну//
+let firstName = 'Taras';
+
+let middleName = 'Mihaylovych';
+
+let lastName = 'Lashchiv';
+
+let myName = `${lastName} ${firstName} ${middleName}`;
+console.log(myName);
+//створив змінні і об'єднав їх за допомогою конкатенації//
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+//визначив типи трьох змінних//
+
+
+
+
+
+
+
+
+
